cd586a37e0f6f8f23086cdc53aa9bda58fc38526
kidcher1412 <kidcher1412@gmail.com>
2022-12-25 02:46:02 +0700

    fix crash load music

--- Music/script.js
+++ Music/script.js
@@ -369,11 +369,12 @@ get(ref(db, `Songs/${UserName}`)).then((snapshot) => {
                   e.json().then(e1 =>{
                     console.log(e1.link)
                     const audio = new Audio(e1.link);
-                    // req.open("GET", audio, true);
-                    // req.responseType = "blob";
-                    // req.send();
-                    // const blob = req.response;
-                    // const file = URL.createObjectURL(blob);
+                    // const req1 = new XMLHttpRequest();
+                    // req1.open("GET", e1.link, true);
+                    // req1.responseType = "blob";
+                    // req1.send();
+                    // const blob1 = req1.response;
+                    // const file = URL.createObjectURL(blob1);
                     song.files[media] = audio;
                     resolve(song);
                   })
